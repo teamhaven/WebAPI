d856a02ff48fc59082c5c08034cfd66a7a542c52
Richard Thombs <richard@teamhaven.com>
2014-05-30 01:06:32 -0700

    AzureQueueAccessInformation models

---
+++ src/TeamHaven.WebApi.Models/AzureQueueAccessInformation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamHaven.WebApi.Models
+{
+	/// <summary>
+	/// Class encapsulating all the information required to create an SAS token
+	/// for accessing a Windows Azure Storage Queue.
+	/// </summary>
+	public class AzureQueueAccessInformation
+	{
+		/// <summary>
+		/// A temporary access token that can be used to access the queue until
+		/// it expires.
+		/// </summary>
+		public string Token { get; set; }
+
+		/// <summary>
+		/// UTC date/time indicating when the temporary access token will expire.
+		/// </summary>
+		public DateTime Expires { get; set; }
+
+		/// <summary>
+		/// The URIs that can be used to access the queue.
+		/// </summary>
+		public StorageUri StorageUri { get; set; }
+
+		/// <summary>
+		/// The name of the queue.
+		/// </summary>
+		public string QueueName { get; set; }
+	}
+
+	/// <summary>
+	/// The URIs that can be used to connect to an Azure Storage Account.
+	/// </summary>
+	public class StorageUri
+	{
+		public string PrimaryUri { get; set; }
+		public string SecondaryUri { get; set; }
+	}
+}
